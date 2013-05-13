31e5a22de8c4b71155e0cac988aa7c0920927de2
DiabloHorn <diablohorn@gmail.com>
2013-05-13 15:10:17 -0700

    added more ip providers

--- misc/myip.py
+++ misc/myip.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 #DiabloHorn http://diablohorn.wordpress.com
-#get external ip using http://ip.nu or display full html response if it fails
+#get external ip using multiple sites
+#yes I know it's horrible to parse html with regex
 
+import sys
 import urllib2
 import re
 
+ipproviders = ('http://ip.nu/','http://my.ip.fi/','http://ip.bsd-unix.net/','http://ip.sidn.nl/')
 ipextract = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
-response = urllib2.urlopen('http://ip.nu/').read()
-if response != None:
-	rematch = ipextract.search(response)
-	if rematch != None:
-		print rematch.group(0)
-	else:
-		print response
+
+def lookupip(addy):
+	response = None
+	try:
+		response = urllib2.urlopen(addy).read()
+	except:
+		print 'Skipping ' + addy + ' due to urllib2 error'
+
+	if response != None:
+		rematch = ipextract.search(response)
+		if rematch != None:
+			print rematch.group(0)
+
+for provider in ipproviders:
+	lookupip(provider)
+	
+
